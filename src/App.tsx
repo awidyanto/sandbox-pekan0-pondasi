720667188890a0e9da49287db6a3d1e3c7275b1e
awidyanto <agungwidyanto@gmail.com>
2025-07-10 18:52:26 +0700

    add typescript

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,16 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
-function App() {
-  // 'useState' adalah cara React untuk mengingat sebuah nilai yang bisa berubah.
-  // Di sini, kita bilang: "React, tolong ingat nilai 'count', awalnya adalah 0".
-  const [count, setCount] = useState(0);
+interface AppProps {}
 
-  // Fungsi untuk menambah nilai count
+function App({}: AppProps) {
+  
+  const [count, setCount] = useState<number>(0);
+
   const increment = () => {
     setCount(count + 1);
   };
 
-  // Fungsi untuk mengurangi nilai count
   const decrement = () => {
     setCount(count - 1);
   };
@@ -31,12 +30,10 @@ function App() {
       <div>
         <h1>Aplikasi Counter</h1>
         <p>Nilai saat ini:</p>
-        {/* Menampilkan nilai 'count' yang diingat React */}
+
         <h2 className="counter-value">{count}</h2>
         <div className="button-group">
-          {/* Saat tombol ini diklik, panggil fungsi 'decrement' */}
           <button onClick={decrement}>- Kurangi</button>
-          {/* Saat tombol ini diklik, panggil fungsi 'increment' */}
           <button onClick={increment}>+ Tambah</button>
         </div>
       </div>
