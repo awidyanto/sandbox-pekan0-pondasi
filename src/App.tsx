4392f07ddbf33e5e2a15a951259c0c5b0d97d281
awidyanto <agungwidyanto@gmail.com>
2025-07-10 19:06:45 +0700

    Styling using Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -17,16 +17,28 @@ function App({}: AppProps) {
 
   return (
     <>
-      <div>
-      <h1>Aplikasi Counter (TS Version)</h1>
-      <p>Nilai saat ini:</p>
+    <div className="bg-slate-900 min-h-screen flex items-center justify-center text-white">
+    <div className="bg-slate-800 p-8 rounded-xl shadow-lg text-center">
+    <h1 className="text-2xl font-bold mb-4">Aplikasi Counter Tailwind</h1>
+        <p className="text-slate-400">Nilai saat ini:</p>
 
-        <h2 className="counter-value">{count}</h2>
-        <div className="button-group">
-          <button onClick={decrement}>- Kurangi</button>
-          <button onClick={increment}>+ Tambah</button>
+        <h2 className="text-7xl font-mono font-bold my-4">{count}</h2>
+        <div className="flex gap-4 justify-center">
+        <button
+            onClick={decrement}
+            className="bg-red-500 px-6 py-2 rounded-lg font-semibold hover:bg-red-600 transition-colors"
+          >
+            - Kurangi
+          </button>
+          <button
+            onClick={increment}
+            className="bg-green-500 px-6 py-2 rounded-lg font-semibold hover:bg-green-600 transition-colors"
+          >
+            + Tambah
+          </button>
         </div>
       </div>
+    </div>
     </>
   );
 }
